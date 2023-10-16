7162bfd745f1e94311a57d1304ee0e34ed7fa469
Vishal Yadav <yadavvi91@gmail.com>
2023-10-16 16:10:28 +0530

    Code from the interview

--- src/test/java/org/yadavvi/LRUCacheTest.java
+++ src/test/java/org/yadavvi/LRUCacheTest.java
@@ -1,7 +1,36 @@
 package org.yadavvi;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LRUCacheTest {
+    @Test
+    public void testCache() {
+        // LRU Cache size is 5
+        // key should return a value
+        Map<String, String> map = new HashMap<>(5);
+        int maxSize = 5;
+        String firstValue = map.get("first");
+        // List kind of thing where once the 6th element is added,
+        // we remove the first element and add the 6th at the end to make it as the new 5th.
+        if (map.size() == 5) {
+
+        }
+
+
+    }
+
+    class LRUCache<K, V> {
+
+    }
+
+    class Model<K, V> {
+        String K;
+        String V;
+    }
 
 }
